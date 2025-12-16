1f2cae8e5dd107085c2cf7e00a7ef883c626fb54
Leo <108789516+Boco14@users.noreply.github.com>
2025-12-16 16:48:13 +0800

    Update: logout/login bug fixed

--- banking_app_cli/src/Action/LoginUser.java
+++ banking_app_cli/src/Action/LoginUser.java
@@ -7,24 +7,34 @@ import java.util.Scanner;
 
 public class LoginUser {
     private static final Scanner _SCAN = new Scanner(System.in);
-    private static  String email = "leo@gmail.com";
-    private static int password = 12345;
-    public static void userLoginPrompt(){
+    private static String email;
+    private static int password;
+
+    public static void userLoginPrompt() {
         System.out.println("Enter your credentials");
-        System.out.println("Enter email: leo@gmail.com");
-        /*email = _SCAN.nextLine();*/
-        try{
-            System.out.println("Enter pin: 12345");
-            /*password = _SCAN.nextInt();*/
-            lineBreakDisplay();
-        }catch (Exception InputMismatchException){
-            inputWarningDisplay();
-            lineBreakDisplay();
+
+        System.out.print("Enter email: ");
+        email = _SCAN.nextLine();
+
+        while (true) {
+            System.out.print("Enter pin: ");
+            String pinInput = _SCAN.nextLine();
+            try {
+                password = Integer.parseInt(pinInput);
+                break;
+            } catch (NumberFormatException e) {
+                inputWarningDisplay();
+                System.out.println("-- Please enter a numeric PIN --");
+            }
         }
+
+        lineBreakDisplay();
     }
-    public static String getEmailLogin(){
+
+    public static String getEmailLogin() {
         return email;
     }
+
     public static int getPassword() {
         return password;
     }
