be7a8304af3b9bc4014df9ce99ee434c3f5ff631
Leo <108789516+Boco14@users.noreply.github.com>
2025-12-18 00:25:28 +0800

    Update: LogonUser.java

--- banking_app_cli/src/Action/LoginUser.java
+++ banking_app_cli/src/Action/LoginUser.java
@@ -2,20 +2,15 @@ package Action;
 
 import static Action.pages.display.UserDisplay.inputWarningDisplay;
 import static Action.pages.display.UserDisplay.lineBreakDisplay;
-
 import java.util.Scanner;
-
 public class LoginUser {
     private static final Scanner _SCAN = new Scanner(System.in);
     private static String email;
     private static int password;
-
     public static void userLoginPrompt() {
         System.out.println("Enter your credentials");
-
         System.out.print("Enter email: ");
         email = _SCAN.nextLine();
-
         while (true) {
             System.out.print("Enter pin: ");
             String pinInput = _SCAN.nextLine();
@@ -27,14 +22,11 @@ public class LoginUser {
                 System.out.println("-- Please enter a numeric PIN --");
             }
         }
-
         lineBreakDisplay();
     }
-
     public static String getEmailLogin() {
         return email;
     }
-
     public static int getPassword() {
         return password;
     }
